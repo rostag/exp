8635d63b90f7a5896c877ab3f206ab0cc7cecc3d
rosty <rostyslav.siryk@gmail.com>
2020-04-17 18:40:09 +0300

    Renamed flow-diagram folter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FlowDiagramComponent } from './diagram/diagram.component';
+import { FlowDiagramComponent } from './flow-diagram/diagram.component';
 
 @NgModule({
   declarations: [
