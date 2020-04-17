8cc9e155012e5ad3497195f138842da143954318
rosty <rostyslav.siryk@gmail.com>
2020-04-17 17:39:27 +0300

    Cleanup

--- src/app/diagram/diagram.component.ts
+++ src/app/diagram/diagram.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, HostListener } from '@angular/core';
 import * as d3 from 'd3';
-import * as d3Sankey from 'd3-sankey';
 
 export interface FlowEntry {
     source: string,
@@ -14,6 +13,81 @@ export interface ResourceGroup {
     label: string,
     type: string
 }
+
+export const flowEntries: FlowEntry[] = [
+    {
+        source: 'source-1',
+        destination: 'destination-1',
+        intent: 'ALLOW',
+        label: 'Internet Users'
+    },
+    {
+        source: 'source-2',
+        destination: 'destination-1',
+        intent: 'ALLOW',
+        label: 'Svc-Svc Blocks'
+    },
+    {
+        source: 'source-3',
+        destination: 'destination-1',
+        intent: 'DENY',
+        label: 'Data Compliance Policy'
+    },
+    {
+        source: 'source-4',
+        destination: 'destination-1',
+        intent: 'ALLOW',
+        label: 'User-Data Requests'
+    },
+    {
+        source: 'source-5',
+        destination: 'destination-1',
+        intent: 'DENY',
+        label: 'Unknown Policy Name'
+    },
+    {
+        source: 'source-6',
+        destination: 'destination-1',
+        intent: 'ALLOW',
+        label: '3rd Part Service Data Ingestion'
+    },
+    {
+        source: 'source-1',
+        destination: 'destination-2',
+        intent: 'ALLOW',
+        label: 'Test Policy 3'
+    },
+    {
+        source: 'source-3',
+        destination: 'destination-2',
+        intent: 'ALLOW',
+        label: 'VIP Data'
+    },
+    {
+        source: 'source-4',
+        destination: 'destination-2',
+        intent: 'ALLOW',
+        label: 'Privileged'
+    },
+    {
+        source: 'source-5',
+        destination: 'destination-2',
+        intent: 'ALLOW',
+        label: 'Default DENY'
+    },
+    {
+        source: 'source-6',
+        destination: 'destination-2',
+        intent: 'ALLOW',
+        label: ''
+    },
+    {
+        source: 'source-3',
+        destination: 'destination-3',
+        intent: 'ALLOW',
+        label: ''
+    }
+];
 
 @Component({
     selector: 'app-diagram',
@@ -136,87 +210,6 @@ export class DiagramComponent implements OnInit {
     }    
 
     private drawChart(evt) {
-
-        const flowEntries: FlowEntry[] = [
-            {
-                source: 'source-1',
-                destination: 'destination-1',
-                intent: 'ALLOW',
-                label: 'Internet Users'
-            },
-            {
-                source: 'source-2',
-                destination: 'destination-1',
-                intent: 'ALLOW',
-                label: 'Internet Users'
-            },
-            {
-                source: 'source-3',
-                destination: 'destination-1',
-                intent: 'DENY',
-                label: 'Internet Users'
-            },
-            {
-                source: 'source-4',
-                destination: 'destination-1',
-                intent: 'ALLOW',
-                label: 'Internet Users'
-            },
-            {
-                source: 'source-5',
-                destination: 'destination-1',
-                intent: 'DENY',
-                label: 'Internet Users'
-            },
-            {
-                source: 'source-6',
-                destination: 'destination-1',
-                intent: 'ALLOW',
-                label: 'Internet Users'
-            },
-            {
-                source: 'source-1',
-                destination: 'destination-2',
-                intent: 'ALLOW',
-                label: ''
-            },
-            {
-                source: 'source-3',
-                destination: 'destination-2',
-                intent: 'ALLOW',
-                label: ''
-            },
-            {
-                source: 'source-4',
-                destination: 'destination-2',
-                intent: 'ALLOW',
-                label: ''
-            },
-            {
-                source: 'source-5',
-                destination: 'destination-2',
-                intent: 'ALLOW',
-                label: ''
-            },
-            {
-                source: 'source-6',
-                destination: 'destination-2',
-                intent: 'ALLOW',
-                label: ''
-            },
-            {
-                source: 'source-3',
-                destination: 'destination-3',
-                intent: 'ALLOW',
-                label: ''
-            },
-            {
-                source: 'source-4',
-                destination: 'destination-10',
-                intent: 'ALLOW',
-                label: ''
-            }
-        ]
 
         // Clean up before re-rendering
         d3.select('.chart-container').selectAll('*').remove();
@@ -361,132 +354,15 @@ export class DiagramComponent implements OnInit {
         }
     }
 
-    private DrawChart() {
-
-        let k = 1;
-
-        var svg = d3.select("#sankey"),
-            width = +svg.attr("width"),
-            height = +svg.attr("height");
-
-        var formatNumber = d3.format(",.0f"),
-            format = function (d: any) { return formatNumber(d) + " TWh"; },
-            color = d3.scaleOrdinal(d3.schemeCategory10);
-
-        var sankey = d3Sankey.sankey()
-            .nodeWidth(15)
-            .nodePadding(10)
-            .extent([[1, 1], [width - 1, height - 6]]);
-
-        var link = svg.append("g")
-            .attr("class", "links")
-            .attr("fill", "none")
-            .attr("stroke", "#000")
-            .attr("stroke-opacity", 0.2)
-            .selectAll("path");
-
-        var node = svg.append("g")
-            .attr("class", "nodes")
-            .attr("font-family", "sans-serif")
-            .attr("font-size", 10)
-            .selectAll("g");
-
-        function getRandomLink(min, max, volume) {
-            const link = {
-                source: min + Math.round(Math.random() * max),
-                target: min + Math.floor(Math.random() * max),
-                value: 1 + Math.floor(Math.random() * volume),
-                uom: 'Widget(s)'
-            }
-
-            if (link.target === link.source) {
-                link.target = link.source > 0 ? link.target - 1 : 1;
-            }
-
-            return link;
-        }
-
-        function getRandomLinks(min, max, linkCount, volume) {
-            const links = [];
-            for (let i = 0; i < linkCount; i++) {
-                links.push(getRandomLink(min, max, volume));
-            }
-            return links;
-        }
-
-        let dataset;
-        let datasets: DAG[] = null;
-
-        function getNodes() {
-            return dataset;
-        }
-
-        function getNodesByCount(nodeCount, linkCount) {
-            const data: DAG = {
-                nodes: [],
-                links: getRandomLinks(0, nodeCount, linkCount, 1)
-            }
-            for (let n = 0; n < nodeCount; n++) {
-                data.nodes.push({
-                    nodeId: n,
-                    name: "N" + n
-                });
-            }
-            return data;
-        }
-
-        link = link
-            .data(dataset.links)
-            .enter().append("path")
-            .attr("d", d3Sankey.sankeyLinkHorizontal())
-            .attr("stroke-width", function (d: any) { return Math.max(1, d.width); });
-
-        link.append("title")
-            .text(function (d: any) { return d.source.name + " → " + d.target.name + "\n" + format(d.value); });
-
-        node = node
-            .data(dataset.nodes)
-            .enter().append("g");
-
-        node.append("rect")
-            .attr("x", function (d: any) { return d.x0; })
-            .attr("y", function (d: any) { return d.y0; })
-            .attr("height", function (d: any) { return d.y1 - d.y0; })
-            .attr("width", function (d: any) { return d.x1 - d.x0; })
-            .attr("fill", function (d: any) { return color(d.name.replace(/ .*/, "")); })
-            .attr("stroke", "#000");
-
-        node.append("text")
-            .attr("x", function (d: any) { return d.x0 - 6; })
-            .attr("y", function (d: any) { return (d.y1 + d.y0) / 2; })
-            .attr("dy", "0.35em")
-            .attr("text-anchor", "end")
-            .text(function (d: any) { return d.name; })
-            .filter(function (d: any) { return d.x0 < width / 2; })
-            .attr("x", function (d: any) { return d.x1 + 6; })
-            .attr("text-anchor", "start");
-
-        node.append("title")
-            .text(function (d: any) { return d.name + "\n" + format(d.value); });
-        //});
+    private testChart() {
+        // node.append("text")
+        //     .attr("x", function (d: any) { return d.x0 - 6; })
+        //     .attr("y", function (d: any) { return (d.y1 + d.y0) / 2; })
+        //     .attr("dy", "0.35em")
+        //     .attr("text-anchor", "end")
+        //     .text(function (d: any) { return d.name; })
+        //     .filter(function (d: any) { return d.x0 < width / 2; })
+        //     .attr("x", function (d: any) { return d.x1 + 6; })
+        //     .attr("text-anchor", "start");
     }
-}
-
-interface SNodeExtra {
-    nodeId: number;
-    name: string;
-}
-
-interface SLinkExtra {
-    source: number;
-    target: number;
-    value: number;
-    uom: string;
-}
-type SNode = d3Sankey.SankeyNode<SNodeExtra, SLinkExtra>;
-type SLink = d3Sankey.SankeyLink<SNodeExtra, SLinkExtra>;
-
-interface DAG {
-    nodes: SNode[];
-    links: SLink[];
 }
